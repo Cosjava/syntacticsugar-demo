915376f71c494d18f3e1543cb38780c27abf181a
Sandrine Banas <sandrine.banas@cgi.com>
2022-02-01 23:14:37 +0100

    Fix small issues

--- init/src/main/java/com/cgi/conferences/models/projections/StatsByPersonneProjection.java
+++ init/src/main/java/com/cgi/conferences/models/projections/StatsByPersonneProjection.java
@@ -1,11 +1,22 @@
 package com.cgi.conferences.models.projections;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
 public class StatsByPersonneProjection {
     private String nom;
     private long nbConferences;
+
+    public long getNbConferences() {
+        return nbConferences;
+    }
+
+    public void setNbConferences(long nbConferences) {
+        this.nbConferences = nbConferences;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
 }
